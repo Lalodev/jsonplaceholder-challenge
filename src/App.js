2f6798a194417b984e4e536b4304d481570a02d1
Lalodev <laloing87@gmail.com>
2022-06-28 13:06:40 -0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Form from './components/Form';
+import List from './components/List';
+import Table from './components/Table';
 
 function App() {
+  const [reqType, setReqType] = useState('users');
+  const [items, setItems] = useState([]);
+
+  const API_URL = 'https://jsonplaceholder.typicode.com/';
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const res = await fetch(`${API_URL}${reqType}`);
+        const data = await res.json();
+        //console.log(data);
+        setItems(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchItems();
+  }, [reqType]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form reqType={reqType} setReqType={setReqType} />
+      {/*<List items={items} />*/}
+      <Table items={items} />
     </div>
   );
 }
